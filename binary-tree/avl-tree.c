67357798cb8d4c863eb0336bc7eb09e9e1f4a914
gabrieldnsilva <gabrieldnsilva@gmail.com>
2024-09-15 06:16:53 -0300

    feat: Função delete adicionada, assim como suas tratativas para manter a arvore binaria devidamente balanceada, adicionadas tratativas no caso de arvore estar vazia e funcao para limpar a arvore

--- binary-tree/avl-tree.c
+++ binary-tree/avl-tree.c
@@ -83,32 +83,185 @@ node *leftRotate(node *nodeUnbalanced) {
 }
 
 node *balanceAfterInsert(node * currentNode, int data, int balance) {
+    //
 
-    //Data inserted is greater than parent right node data, and right node height is more then 1
+    //  Data inserted is greater than parent right node data, and right node height is more then 1
     if (balance < -1 && data > currentNode->right->data)
         return leftRotate(currentNode);
 
-    //Data inserted is less than parent left node data, and left node height is more than 1
+    //  Data inserted is less than parent left node data, and left node height is more than 1
     if (balance > 1 && data < currentNode->left->data)
         return rightRotate(currentNode);
 
-    //Data inserted is greater than parent left node data, and right node height is more than 1
+    //  Data inserted is greater than parent left node data, and right node height is more than 1
     if (balance > 1 && data > currentNode->left->data) {
         currentNode->left = leftRotate(currentNode->left);
         return rightRotate(currentNode);
     }
 
-    //Data inserted is less than parent right node data, and right node height is more than 1
+    //  Data inserted is less than parent right node data, and right node height is more than 1
     if (balance < -1 && data < currentNode->right->data) {
         currentNode->right = rightRotate(currentNode->right);
         return leftRotate(currentNode);
     }
 
-    //If node is balanced, no need balance
+    //  If node is balanced, no need balance
     return currentNode;
-
-
 }
+    node * balanceAfterDelete(node *currentNode, int balance){
+
+        //  Simple right rotation
+        if(balance > 1 && balanceFactor(currentNode->left) >= 0)
+            currentNode = rightRotate(currentNode);
+
+        //  Double rotation - left right
+        if (balance > 1 && balanceFactor(currentNode->left) < 0) {
+            currentNode->left = leftRotate(currentNode->left);
+            currentNode = rightRotate(currentNode);
+        }
+
+        //  Simple left rotation
+        if (balance < -1 && balanceFactor(currentNode->right) <= 0)
+                currentNode = leftRotate(currentNode);
+
+        //  Double rotation - right left
+        if (balance < -1 && balanceFactor(currentNode->right) > 0) {
+            currentNode->right = rightRotate(currentNode->right);
+            currentNode = leftRotate(currentNode);
+        }
+
+        //If the node keeps balanced after delete
+        return currentNode;
+    }
+
+    node * insert(node *currentNode, int data){
+        if (currentNode == NULL) {
+            //  Creates the new node
+            currentNode = reset();
+            if(currentNode == NULL) {
+                printf("\n ERROR! Memory allocation problem");
+                exit (-1);
+            }
+
+            //  Set the new node data
+            currentNode->data = data;
+        }
+
+        if (currentNode->data > data) {
+
+            //  Call the insert function, that some point of the recursion will return a node
+            //  And this node will be setted at the left of the current node
+            currentNode->left = insert(currentNode->left, data);
+        }
+        else if(currentNode->data < data) {
+
+            //  Same case as previous, but the node will be setted to the right of current node
+            currentNode->right = insert(currentNode->right, data);
+        }
+
+        int balance = balanceFactor(currentNode);
+        currentNode = balanceAfterInsert(currentNode, data, balance);
+        return currentNode;
+    }
+
+    node * minValueNode(node * root) {
+    node *currentNode = root;
+
+    while(currentNode && currentNode->left != NULL)
+        currentNode = currentNode->left;
+
+    return currentNode;
+}
+
+    node * delete(node *currentNode, int data){
+        if(currentNode == NULL)
+            return currentNode;
+
+        /**
+         *  If the Data to be Deleted is smaller
+         *  than the currentNode data
+         *  then it go to the left subtree
+         */
+        if (data < currentNode->data)
+            currentNode->left = delete(currentNode->left, data);
+
+        /**
+         *  If data to be deleted is greater
+         *  than the currentNode data
+         *  then it go to the right subtree
+         */
+        else if(data > currentNode->data)
+            currentNode->right = delete(currentNode->right, data);
+
+        /**
+         *  if data is equal to the currentNode data,
+         *  then currentNode is the one to be deleted.
+         */
+        else if(data == currentNode->data) {
+            if(currentNode->left == NULL) { //in the left case
+                node *temp = currentNode->right;
+                free(currentNode);
+                return temp;
+            }
+
+        else if (currentNode-> right == NULL) { //in the right case
+            node *temp = currentNode->left;
+            free(currentNode);
+            return temp;
+        }
+
+        //  node with two children:
+        //  Get sucessor - (smallest in the right subtree)
+        node *temp = minValueNode(currentNode->right);
+
+        //  Copy the sucessor's data to currentNode
+        currentNode->data = temp->data;
+
+        //  Delete the sucessor node
+        currentNode->right = delete(currentNode->right, temp->data);
+        }
+
+        int balance = balanceFactor(currentNode);
+        currentNode = balanceAfterDelete(currentNode, balance);
+        return currentNode;
+    }
+
+    int empty(node *root) {
+    if (root  == NULL) {
+        printf("\n WARN: Empty Tree");
+        return 1;
+    }
+    return 0;
+}
+
+    void clear(node *root) {
+        node *currentNode = root;
+
+        if (currentNode != NULL) {
+            clear(currentNode->left);
+            clear(currentNode->right);
+        }
+        free(currentNode);
+
+        return;
+    }
+
+    void print(node *root, int level) {
+        node *currentNode = root;
+
+        if(currentNode != NULL) {
+
+            printf("\n LEVEL: %d - %d", level, currentNode->data);
+
+            print(currentNode->left, level + 1);
+            print(currentNode->right, level + 1);
+        }
+
+        return;
+}
+
+
+
 
 int main(void) {
     printf("Hello, World!\n");
